38262850001f24ec79de7fd8bc20fdb978b90000
Marivanenoronha <noronhamarivane4@gmail.com>
2023-06-25 23:21:55 -0300

    add function meesageAvarege in the file

--- scripts.js
+++ scripts.js
@@ -20,5 +20,17 @@ function calculteAvarege(noteOne, noteTwo, noteOptional) {
     return avarege
 }
 
+function messageAvarege(avarege) {
+    if(avarege >= 6) {
+        console.log(`Approved: ${avarege}`)
+    }else if(avarege < 3) {
+        console.log(`Disapproved: ${avarege}`)
+    }else{
+        console.log(`Exam: ${avarege}`)
+    }
+}
+
 const avaregeStudentOne = calculteAvarege(assessmentOne, assessmentTwo, assessmentOptional)
 const avaregeStudentTwo = calculteAvarege(2, 5, 7)
+messageAvarege(avaregeStudentOne)
+messageAvarege(avaregeStudentTwo)
