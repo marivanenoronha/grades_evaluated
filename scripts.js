e49d4b8aa9ddabb3fafb3eaa9c616958637fcdb0
Marivanenoronha <noronhamarivane4@gmail.com>
2023-06-25 23:00:49 -0300

    add function calculateAvarege in the file

--- scripts.js
+++ scripts.js
@@ -8,3 +8,15 @@ if(assessmentOptional == undefined) {
     assessmentOptional = -1
     console.log(assessmentOptional)
 }
+
+function calculteAvarege(noteOne, noteTwo, noteOptional) {
+    if(noteOne < noteTwo){
+        noteOne = noteOptional
+    }else if(noteTwo < noteOne) {
+        noteTwo = noteOptional
+    }
+
+    console.log(noteOne, noteTwo, noteOptional)
+}
+
+calculteAvarege(assessmentOne, assessmentTwo, assessmentOptional)
