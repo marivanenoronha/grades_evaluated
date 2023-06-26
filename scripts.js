b90b43f87b3c8a3bb212c35dbcb92c5d0138bcef
Marivanenoronha <noronhamarivane4@gmail.com>
2023-06-25 22:51:13 -0300

    add const and if in the file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 console.log("Start of application")
+
+const assessmentOne = 4
+const assessmentTwo = 8
+let assessmentOptional = 2
+
+if(assessmentOptional == undefined) {
+    assessmentOptional = -1
+    console.log(assessmentOptional)
+}
